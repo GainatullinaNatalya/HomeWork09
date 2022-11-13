dca281f5a8b90cc39a67b760ab2d6e394f10007b
Nate <etlf74@yandex.ru>
2022-11-13 21:18:04 +0500

    Added Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int SumNumbers(int m, int n)
+{
+    if(m==n) return m;
+    else if (m < n) return n + SumNumbers(m, n-1);
+    else return n + SumNumbers(m, n+1);
+}
+
+int ReadIntegerNumber(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int m = ReadIntegerNumber("Введите натуральное число М: ");
+int n = ReadIntegerNumber("Введите натуральное число N: ");
+Console.WriteLine($"Сумма элементов от {m} до {n} = " + SumNumbers(m, n));
