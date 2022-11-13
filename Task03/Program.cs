b195b4ab127af402565f7db2b71791d7f6f24447
Nate <etlf74@yandex.ru>
2022-11-13 21:18:11 +0500

    Added Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу вычисления функции Аккермана
+
+int ReadIntegerNumber(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int AckermannFunction (int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return AckermannFunction(m - 1, 1);
+    if (m > 0 && n > 0) return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+return AckermannFunction(m, n);
+}
+
+int m = ReadIntegerNumber("Введите натуральное число М: ");
+int n = ReadIntegerNumber("Введите натуральное число N: ");
+Console.WriteLine(AckermannFunction(m, n));
