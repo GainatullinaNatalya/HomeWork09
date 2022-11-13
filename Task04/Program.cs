ca2a2a26c5e9bc5dcb29c0e574883c20b09e8c60
Nate <etlf74@yandex.ru>
2022-11-13 21:18:22 +0500

    Added Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+int ExpoNumb (int a, int b)
+{
+    if(b == 0) return 1;
+    else return a * ExpoNumb(a, b - 1);
+}
+
+int ReadIntegerNumber(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int a = ReadIntegerNumber("Введите число A: ");
+
+int b = ReadIntegerNumber("Введите степень числа: ");
+
+Console.WriteLine($"Число {a} в степени {b} равно " + ExpoNumb(a, b));
